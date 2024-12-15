0870a238ea705e895c98f66ce0465bf87b9bdba1
ahmedhesein1 <ahmedhesein1790@gmail.com>
2024-12-15 21:16:52 +0200

    Initial Commit

---
+++ src/routes/menuRoutes.js
@@ -0,0 +1,36 @@
+import express from "express";
+import * as menuController from "../controllers/menuController.js";
+import * as authController from "../controllers/authController.js";
+
+const router = express.Router();
+
+router
+  .route("/")
+  .post(
+    authController.protect,
+    authController.authorize(["admin"]),
+    menuController.createMenuItem
+  ) // Admin only
+  .get(menuController.getMenuItems); // Public route
+
+router
+  .route("/:id")
+  .get(menuController.getMenuItemById) // Public route
+  .patch(
+    authController.protect,
+    authController.authorize(["admin"]),
+    menuController.updateMenuItem
+  ) // Admin only
+  .delete(
+    authController.protect,
+    authController.authorize(["admin"]),
+    menuController.deleteMenuItem
+  ); // Admin only
+router.post(
+  "/bulk",
+  authController.protect,
+  authController.authorize(["admin"]),
+  menuController.createBulkMenus
+); // Admin only
+
+export default router;
